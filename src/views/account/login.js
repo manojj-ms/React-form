b74561a652b8b9a522e07d08335f4bdd3cc3cb0e
leo phenomenal <leo@phenomenaltech.com>
2021-06-09 20:12:43 +0530

    form-api-integration

--- src/views/account/login.js
+++ src/views/account/login.js
@@ -1,46 +1,65 @@
-import react from 'react';
-import { useForm } from 'react-hook-form'; 
-import { Link } from 'react-router-dom'
-
-const Login = (props) =>{
-const { register,handleSubmit,errors } = useForm();
+import { useForm } from 'react-hook-form';
+import { Link, useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 
-return (
-    <div>
-<section class="min-h-screen flex flex-col">
-            <div class="flex flex-1 items-center justify-center">
-                <div class="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
-                    <form class="text-center">
-                        <h1 class="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
-                            Sign in
+const Login = (props) => {
+    const { register, handleSubmit } = useForm();
+    const history = useHistory();
+    const onSubmit = (values) => {
+        const body = {
+            email: values.email.toLowerCase(),
+            password: values.password,
+        };
+        axios.post(`https://reqres.in/api/login`, body)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                {/*console.log(values);*/ }
+                history.push('/dashboard');
+            })
+
+    }
+
+    return (
+        <div>
+            <section className="min-h-screen flex flex-col">
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
+                        <form className="text-center" onSubmit={handleSubmit(onSubmit)}>
+                            <h1 className="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
+                                Sign in
                         </h1>
-                        <div class="py-2 text-left">
-                            <input type="email" class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Email" required />
+                            <div className="py-2 text-left">
+                                <input type="email" className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Email"
+                                    {...register('email', { required: true })}
+                                />
+                            </div>
+                            <div class="py-2 text-left">
+                                <input type="password" className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Password"
+                                    {...register('password', { required: true })}
+                                />
+                            </div>
+                            <div class="py-2">
+                                <button type="submit" className="border-2 border-gray-100 focus:outline-none bg-purple-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg focus:border-gray-700 hover:bg-purple-700">
+                                    <Link to='/dashboard'>Sign In</Link>
+                                </button>
+                            </div>
+                        </form>
+                        <div className="text-center">
+                            <a href="#" className="hover:underline">Forgot password?</a>
                         </div>
-                        <div class="py-2 text-left">
-                            <input type="password" class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Password" required />
-                        </div>
-                        <div class="py-2">
-                            <button type="submit" class="border-2 border-gray-100 focus:outline-none bg-purple-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg focus:border-gray-700 hover:bg-purple-700">
-                                Sign In
-                            </button>
-                        </div>
-                    </form>
-                    <div class="text-center">
-                        <a href="#" class="hover:underline">Forgot password?</a>
-                    </div>
-                    <div class="text-center mt-12">
-                        <span>
-                            Don't have an account?
+                        <div className="text-center mt-12">
+                            <span>
+                                Don't have an account?
                         </span>
-                        <a href="#" class="font-light text-md text-indigo-600 underline font-semibold hover:text-indigo-800"><Link to='/register'>Create One</Link></a>
+                            <a href="#" className="font-light text-md text-indigo-600 underline font-semibold hover:text-indigo-800"><Link to='/register'>Create One</Link></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
         </div>
     )
-  }
-    
+}
+
 export default (Login);
--- src/views/account/register.js
+++ src/views/account/register.js
@@ -1,55 +1,90 @@
-import react from 'react';
-import { useForm } from 'react-hook-form'; 
-import { Link } from 'react-router-dom'
-
-const Register = (props) =>{
-const { register,handleSubmit,errors } = useForm();
+import { useForm } from 'react-hook-form';
+import { Link, useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 
-return (
-    <div>
-    <section class="min-h-screen flex flex-col">
-              
-                <div class="flex flex-1 items-center justify-center">
-                    <div class="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
-                        <form class="text-center">
-                            <h1 class="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
+
+const Register = (props) => {
+    const { register, handleSubmit } = useForm();
+    const history = useHistory();
+    const onSubmit = (values) => {
+        const body = {
+            f_name: values.f_name,
+            l_name: values.l_name,
+            email: values.email.toLowerCase(),
+            password: values.password,
+            confirmpassword: values.confirmpassword,
+        };
+        axios.post(`https://reqres.in/api/users`, body)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                {/*console.log(values);*/ }
+                history.push('/login');
+            })
+
+    }
+
+
+    return (
+        <div>
+            <section className="min-h-screen flex flex-col">
+
+                <div className="flex flex-1 items-center justify-center">
+                    <div className="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
+                        <form className="text-center" onSubmit={handleSubmit(onSubmit)}>
+                            <h1 className="font-bold tracking-wider text-3xl mb-8 w-full text-gray-600">
                                 Register
                             </h1>
-                            <div class="py-2 text-left">
-                                <input type="name" class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Name" required />
+
+                            <div className="py-2 text-left">
+                                <input type="name" name="f_name" className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
+                                    placeholder="First Name"
+                                    {...register('name', { required: true })}
+                                />
                             </div>
-                            <div class="py-2 text-left">
-                                <input type="phone" class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Phone" required/>
+
+                            <div className="py-2 text-left">
+                                <input type="name" name="l_name" className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
+                                    placeholder="Last Name"
+                                    {...register('name', { required: true })}
+                                />
                             </div>
-                            <div class="py-2 text-left">
-                                <input type="email" class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Email" required/>
+                            <div className="py-2 text-left">
+                                <input type="email" name="email" className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Email"
+                                   {...register('email', { required: true })}
+                                />
                             </div>
-                            <div class="py-2 text-left">
-                                <input type="password" class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Password" required/>
+                            <div className="py-2 text-left">
+                                <input type="password" name="password" className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Password"
+                                    {...register('password', { required: true })}
+                                />
                             </div>
-                            <div class="py-2 text-left">
-                                <input type="password" class="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 " placeholder="Confirm Password" required />
+                            <div className="py-2 text-left">
+                                <input type="password" name="confirmpassword" className="bg-gray-200 border-2 border-gray-100 focus:outline-none bg-gray-100 block w-full py-2 px-4 rounded-lg focus:border-gray-700 "
+                                    placeholder="Confirm Password"
+                                    {...register('password', { required: true })}
+                                />
                             </div>
-                            <div class="py-2">
-                                <button type="submit" class="border-2 border-gray-100 focus:outline-none bg-purple-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg focus:border-gray-700 hover:bg-purple-700">
-                                    Sign Up
+                            <div className="py-2">
+                                <button type="submit" className="border-2 border-gray-100 focus:outline-none bg-purple-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg focus:border-gray-700 hover:bg-purple-700">
+                                    <Link to='/dashboard'> Sign Up </Link>
                                 </button>
                             </div>
                         </form>
-                        
-                        <div class="text-center mt-12">
+
+                        <div className="text-center mt-12">
                             <span>
                                 Already have an account?
                             </span>
-                            <a href="#" class="font-light text-md text-indigo-600 underline font-semibold hover:text-indigo-800"><Link to='/login'>Sign in</Link></a>
+                            <a href="#" className="font-light text-md text-indigo-600 underline font-semibold hover:text-indigo-800"><Link to='/login'>Sign in</Link></a>
                         </div>
                     </div>
                 </div>
             </section>
-    
+
         </div>
     )
-  }
-    
+}
+
 export default (Register);
