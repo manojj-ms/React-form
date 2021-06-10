0b6f38345abfb9b29b95eb25233b237de1d26cb5
leo phenomenal <leo@phenomenaltech.com>
2021-06-10 15:25:30 +0530

    API Integration

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import  Register  from "./views/account/register";
 import  Login  from "./views/account/login";
 import  Dashboard  from "./views/dashboard/index";
+import { Provider } from 'react-redux'
+import { store } from "./store";
+import { connect } from 'react-redux';
+
+
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Provider store={store}>
         <Router>
           <Switch>
           {/*Authentication*/}
@@ -15,7 +21,7 @@ export default class App extends Component {
             <Route path='/dashboard' component={Dashboard} />
           </Switch>
         </Router>
-        
+        </Provider>
       </div>
     )
   }
--- src/views/account/login.js
+++ src/views/account/login.js
@@ -42,7 +42,7 @@ const Login = (props) => {
                             </div>
                             <div class="py-2">
                                 <button type="submit" className="border-2 border-gray-100 focus:outline-none bg-purple-600 text-white font-bold tracking-wider block w-full p-2 rounded-lg focus:border-gray-700 hover:bg-purple-700">
-                                    <Link to='/dashboard'>Sign In</Link>
+                                Sign In
                                 </button>
                             </div>
                         </form>
